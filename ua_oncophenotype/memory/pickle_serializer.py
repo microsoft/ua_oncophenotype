c8a462738dbe82e794403aa0e2f5ec5534af2989
Sam Preston <sam.preston@microsoft.com>
2025-03-27 05:58:08 -0700

    fix for backwards compatibility with memories pickled from rwd_llm package

--- ua_oncophenotype/memory/pickle_serializer.py
+++ ua_oncophenotype/memory/pickle_serializer.py
@@ -1,5 +1,26 @@
 import base64
 import pickle
+import io
+
+
+class RenameUnpickler(pickle.Unpickler):
+    """Support backwards compatibility with old rwd_llm package name"""
+
+    def find_class(self, module, name):
+        if module.startswith("rwd_llm"):
+            new_module = "ua_oncophenotype" + module[len("rwd_llm") :]
+            print(f"changed module {module} -> {new_module}")
+            module = new_module
+
+        return super().find_class(module, name)
+
+
+def rename_loads(data: bytes):
+    """Load the pickle data (as bytes) with the custom unpickler."""
+    # Wrap 'data' in a file-like object
+    file_like = io.BytesIO(data)
+    # Then use RenameUnpickler to load
+    return RenameUnpickler(file_like).load()
 
 
 class PickleSerializer:
@@ -13,4 +34,4 @@ class PickleDeserializer:
     def __call__(self, obj):
         base64_bytes = obj.encode("utf-8")
         pickled_data = base64.b64decode(base64_bytes)
-        return pickle.loads(pickled_data)
+        return rename_loads(pickled_data)
